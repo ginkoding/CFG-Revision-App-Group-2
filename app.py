cd8c482475c5d990f7e6b91135ecf2d78cf572e2
e-makinde <100035099+e-makinde@users.noreply.github.com>
2022-07-27 20:21:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, url_for, request, flash
 app = Flask(__name__, template_folder='template')
 app.secret_key = 'key'
 
-
 @app.route("/")
 def home():
     return render_template('home.html')
@@ -14,7 +13,7 @@ def login():
     print(data)
     return render_template("login.html")
 
-
+#Sign-up page. Able to GET and POST data. 
 @app.route('/sign-up',  methods = ['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
@@ -22,7 +21,9 @@ def sign_up():
         firstname = request.form.get('firstname')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-
+        
+        #Flashes a warning message if the email or name is too short. Flashes a warning at the top of the page if the passwords don't match.
+        #Show a confirmation message if account is successfully created.
         if len(email) < 4:
             flash('Email must be greater than 4 characters', category='error')
         elif len(firstname) <2:
@@ -30,7 +31,7 @@ def sign_up():
         elif password1 != password2:
             flash('Passwords do not match', category = 'error')
         else:
-            flash('Account created', category = 'success')
+            flash('Account created. Please sign in', category = 'success')
 
 
     return render_template("sign_up.html")
